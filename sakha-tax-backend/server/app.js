a49408c093514eb5752724eb20eba4125759b8aa
Hassan chhipa <forhassan43@gmail.com>
2026-07-30 00:11:24 +0530

    Fix email problem

--- sakha-tax-backend/server/app.js
+++ sakha-tax-backend/server/app.js
@@ -10,6 +10,7 @@ import { apiLimiter } from './middleware/rateLimiter.js'
 import { notFound, errorHandler } from './middleware/errorHandler.js'
 
 const app = express()
+app.set('trust proxy', 1)
 
 // Security headers
 app.use(helmet())
